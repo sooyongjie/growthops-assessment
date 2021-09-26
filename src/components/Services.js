c6c2d638530c59f4531b56e8fdfcb81bca7bfc15
sooyongjie <sooyongjie@gmail.com>
2021-09-26 18:05:50 +0800

    Added services offered section

---
+++ src/components/Services.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+function Services() {
+    return (
+        <div className="services container">
+            <div className="heading">
+                <div className="title">
+                    <div className="lines">
+                        <div className=""></div>
+                        <div className=""></div>
+                    </div>
+                    <h2>WHAT WE <span>OFFER</span></h2>
+                    <div className="lines">
+                        <div className=""></div>
+                        <div className=""></div>
+                    </div>
+                </div>
+                <p className="subtitle">We offer our customers the best services & solutions, this is our list of main services</p>
+            </div>
+            <div className="feature-list">
+                <div className="feature">
+                    <div className="feature-icon">
+                        <i class="fas fa-desktop"></i>
+                    </div>
+                    <div className="feature-name">RESPONSIVE & MULTIPURPOSE</div>
+                    <p className="feature-desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim distinctio fugiat a quisquam ad!</p>
+                    <button>
+                        <span>READ MORE</span>
+                    </button>
+                </div>
+                <div className="feature">
+                    <div className="feature-icon">
+                        <i class="fas fa-desktop"></i>
+                    </div>
+                    <div className="feature-name">RESPONSIVE & MULTIPURPOSE</div>
+                    <p className="feature-desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim distinctio fugiat a quisquam ad!</p>
+                    <button>
+                        <span>READ MORE</span>
+                    </button>
+                </div>
+                <div className="feature">
+                    <div className="feature-icon">
+                        <i class="fas fa-desktop"></i>
+                    </div>
+                    <div className="feature-name">RESPONSIVE & MULTIPURPOSE</div>
+                    <p className="feature-desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim distinctio fugiat a quisquam ad!</p>
+                    <button>
+                        <span>READ MORE</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Services
--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import Nav from './components/Nav';
 import Welcome from './components/Welcome';
-import reportWebVitals from './reportWebVitals';
+import Nav from './components/Nav';
+import Services from './components/Services';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
     <Welcome />
     <Nav />
+    <Services />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
