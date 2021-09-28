3283eb7634a6b60fa87a9289f6bf89eee7c7fed6
sooyongjie <sooyongjie@gmail.com>
2021-09-28 17:16:11 +0800

    SO RESPONSIVE 🔥🔥

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,33 +5,38 @@ function Nav() {
     return (
         <nav>
             <img src={logo} alt="" />
-            <ul>
-                <a href="">
-                    <li>HOME</li>
-                </a>
-                <a href="" className="active">
-                    <li>PORTFOLIO</li>
-                </a>
-                <a href="">
-                    <li>BLOG</li>
-                </a>
-                <a href="">
-                    <li>PAGES</li>
-                </a>
-                <a href="">
-                    <li>FEATURES</li>
-                </a>
-                <a href="">
-                    <li>FEATURES</li>
-                </a>
-                <a href="">
-                    <li>MEGA MENU</li>
-                </a>
-                <a href="">
-                    <li>CONTACT</li>
-                </a>
-            </ul>
-            <i className="fas fa-search" />
+            <div>
+                <ul>
+                    <a href="">
+                        <li>HOME</li>
+                    </a>
+                    <a href="" className="active">
+                        <li>PORTFOLIO</li>
+                    </a>
+                    <a href="">
+                        <li>BLOG</li>
+                    </a>
+                    <a href="">
+                        <li>PAGES</li>
+                    </a>
+                    <a href="">
+                        <li>FEATURES</li>
+                    </a>
+                    <a href="">
+                        <li>FEATURES</li>
+                    </a>
+                    <a href="">
+                        <li>MEGA MENU</li>
+                    </a>
+                    <a href="">
+                        <li>CONTACT</li>
+                    </a>
+                    <i className="fas fa-search" />
+                </ul>
+                <button>
+                    <i class="fas fa-bars"></i>
+                </button>
+            </div>
         </nav>
     )
 }
