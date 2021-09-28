13d615ea68ccb251cc950aa3895711e01c735cb0
sooyongjie <sooyongjie@gmail.com>
2021-09-28 17:43:21 +0800

    Added placeholder to textarea

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,9 +7,9 @@ function Form() {
                 Register Your Interest
             </h1>
             <form action="https://formspree.io/f/mknkewbv" method="post">
-                <input type="text" id="input-name" name="name" placeholder="name" required="required" />
-                <input type="email" id="input-email" name="email" placeholder="email" required="required" />
-                <textarea id="input-message" name="message" required="required"></textarea>
+                <input type="text" id="input-name" name="name" placeholder="Name" required="required" />
+                <input type="email" id="input-email" name="email" placeholder="Email" required="required" />
+                <textarea id="input-message" name="message" placeholder="Message" required="required"></textarea>
                 <select name="abc" id="input-occupation" value="Full-time Parents" required="required">
                     <option value="Full-time Parents">Full-time Parents</option>
                     <option value="Full-time Parents">Part-time Parents</option>
@@ -17,7 +17,7 @@ function Form() {
                     <option value="Full-time Parents">Singe Mom</option>
                     <option value="Full-time Parents">No</option>
                 </select>
-                <input type="telnum" id="input-telephone" name="phone" placeholder="phone" required="required" />
+                <input type="telnum" id="input-telephone" name="phone" placeholder="Phone" required="required" />
                 <button id="submit-form-btn" type="submit">
                     <span>SUBMIT MESSAGE</span>
                 </button>
