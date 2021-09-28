85f5b092915cef844f8d71b0cbfd38bf09757a8f
sooyongjie <sooyongjie@gmail.com>
2021-09-28 15:46:14 +0800

    Updated radial progress bar thingy to show the actual percentage

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,43 +4,61 @@ import Heading from './Heading'
 function Skills() {
     return (
         <div className="skills-container container">
-            <Heading theme="dark" text="OUR POWERFUL " highlight="SKILLS" subtitle="We're good and experienced at different things and areas and we promise about quality of our work"/>
+            <Heading theme="dark" text="OUR POWERFUL " highlight="SKILLS" subtitle="We're good and experienced at different things and areas and we promise about quality of our work" />
             <div className="skills">
-                <div className="skill">
-                    <div className="skill-percentage">
-                        <p>75<span>%</span></p>
+                <div class="skill progress-45 style-2">
+                    <span class="label">45<span class="smaller">%</span></span>
+                    <div class="pie">
+                        <div class="left-side half-circle"></div>
+                        <div class="right-side half-circle"></div>
                     </div>
-                    <div className="skill-name">Web Design</div>
+                    <div class="shadow"></div>
+                    <div className="skill-name">WEB DESIGN</div>
                 </div>
-                <div className="skill">
-                    <div className="skill-percentage">
-                        <p>75<span>%</span></p>
+                <div class="skill progress-92 style-2">
+                    <span class="label">92<span class="smaller">%</span></span>
+                    <div class="pie">
+                        <div class="left-side half-circle"></div>
+                        <div class="right-side half-circle"></div>
                     </div>
-                    <div className="skill-name">Web Development</div>
+                    <div class="shadow"></div>
+                    <div className="skill-name">WEB DESIGN</div>
                 </div>
-                <div className="skill">
-                    <div className="skill-percentage">
-                        <p>75<span>%</span></p>
+                <div class="skill progress-69 style-2">
+                    <span class="label">69<span class="smaller">%</span></span>
+                    <div class="pie">
+                        <div class="left-side half-circle"></div>
+                        <div class="right-side half-circle"></div>
                     </div>
-                    <div className="skill-name">Web Design</div>
+                    <div class="shadow"></div>
+                    <div className="skill-name">WEB DESIGN</div>
                 </div>
-                <div className="skill">
-                    <div className="skill-percentage">
-                        <p>75<span>%</span></p>
+                <div class="skill progress-100 style-2">
+                    <span class="label">100<span class="smaller">%</span></span>
+                    <div class="pie">
+                        <div class="left-side half-circle"></div>
+                        <div class="right-side half-circle"></div>
                     </div>
-                    <div className="skill-name">Web Development</div>
+                    <div class="shadow"></div>
+                    <div className="skill-name">WEB DESIGN</div>
                 </div>
-                <div className="skill">
-                    <div className="skill-percentage">
-                        <p>75<span>%</span></p>
+                <div class="skill progress-83 style-2">
+                    <span class="label">83<span class="smaller">%</span></span>
+                    <div class="pie">
+                        <div class="left-side half-circle"></div>
+                        <div class="right-side half-circle"></div>
                     </div>
-                    <div className="skill-name">Web Design</div>
+                    <div class="shadow"></div>
+                    <div className="skill-name">WEB DESIGN</div>
                 </div>
-                <div className="skill">
-                    <div className="skill-percentage">
-                        <p>75<span>%</span></p>
+                <div class="skill progress-50 style-2">
+                    <span class="label">50<span class="smaller">%</span></span>
+                    <div class="pie">
+                        <div class="left-side half-circle"></div>
+                        <div class="right-side half-circle"></div>
                     </div>
-                    <div className="skill-name">Web Development</div>
+                    <div class="shadow"></div>
+                    <div className="skill-name">WEB DESIGN</div>
                 </div>
             </div>
         </div>
